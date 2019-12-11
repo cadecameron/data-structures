5b2eb1bd734e4e13fe0d0d41688f8d2a85db28c0
cadecameron <cade.cameron@gmail.com>
2019-12-11 15:00:35 -0800

    Completed functional and functional-shared classes

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function () {
+  var newInstance = {};
+  newInstance.storage = {};
+  extend(newInstance, stackMethods);
+  return newInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function (value) {
+    this.storage[Object.keys(this.storage).length] = value;
+  },
 
+  pop: function () {
+    var storageReferences = this.storage;
+    if (Object.keys(storageReferences).length > 0) {
+      var currentFirst = storageReferences[Object.keys(storageReferences).length - 1];
+      delete storageReferences[Object.keys(storageReferences).length - 1];
+      return currentFirst;
+    }
+
+  },
+
+  size: function () {
+    return Object.keys(this.storage).length;
+  },
+};
+
+var extend = function(ob1, ob2) {
+  for (let key in ob2) {
+    ob1[key] = ob2[key];
+  }
+};
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,24 @@
-var Stack = function() {
+var Stack = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
-  someInstance.push = function(value) {
+  someInstance.push = function (value) {
+    storage[Object.keys(storage).length] = value;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function () {
+    if (Object.keys(storage).length > 0) {
+      var currentFirst = storage[Object.keys(storage).length - 1];
+      delete storage[Object.keys(storage).length - 1];
+      return currentFirst;
+    }
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
