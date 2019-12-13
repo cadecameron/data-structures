ec759a5efb40409f2dcbb2c021db197237b4d573
Benjamin Kidder <r.ben.kidder@gmail.com>
2019-12-13 13:26:40 -0800

    Create a linked list with a functional class

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.tail === null) {
+      list.tail = newNode;
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    };
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var container = list.head;
+      list.head = list.head.next;
+      return container.value;
+    }
+
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      };
+    }
+    return false;
   };
 
   return list;
@@ -26,4 +49,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addToTail = O(1)
+ removeHead = O(1)
+ contains = O(n)
  */
