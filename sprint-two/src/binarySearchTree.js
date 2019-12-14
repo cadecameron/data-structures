f47c1d31c24f771907d4ab3bebd466ec09af8505
Benjamin Kidder <r.ben.kidder@gmail.com>
2019-12-14 14:58:25 -0800

    Create binarysearch tree using functional class with duplicate methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,61 @@
-var BinarySearchTree = function(value) {
-};
+var BinarySearchTree = function (value) {
+  var obj = {};
+  obj.left;
+  obj.right;
+  obj.value = value;
 
+  obj.insert = function (inputValue) {
+    if (obj.value > inputValue) {
+      if (obj.left !== undefined) {
+        obj.left.insert(inputValue);
+      } else {
+        obj.left = new BinarySearchTree(inputValue);
+      }
+    } else if (obj.value < inputValue) {
+      if (obj.right !== undefined) {
+        obj.right.insert(inputValue);
+      } else {
+        obj.right = new BinarySearchTree(inputValue);
+      }
+    }
+  };
+
+  obj.contains = function (inputValue) {
+    var found = false;
+    //console.log("Currently checking for ", inputValue)
+
+
+    var searchTree = function (tree) {
+      if (tree.value === inputValue) {
+        found = true;
+        console.log("Found true value!");
+        return;
+      } else {
+        if ((inputValue < tree.value) && (tree.left !== undefined)) {
+          searchTree(tree.left);
+        } else if ((inputValue > tree.value) && (tree.right !== undefined)) {
+          searchTree(tree.right);
+        }
+      }
+    }
+    searchTree(this);
+    return found;
+  };
+
+  obj.depthFirstLog = function (cb) {
+    cb(obj.value);
+
+    if (obj.left) {
+      obj.left.depthFirstLog(cb)
+    }
+    if (obj.right) {
+      obj.right.depthFirstLog(cb)
+    }
+
+  };
+  return obj;
+
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
