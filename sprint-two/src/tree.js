7653559bfbcf17b4e7c821dd42b7f070407b3cda
Benjamin Kidder <r.ben.kidder@gmail.com>
2019-12-13 19:17:23 -0800

    Create a tree with functional class with shared methods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,10 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+
+  // children property contains an array with a number of subtrees
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,13 +14,29 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newTree = Tree(value);
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target) {
+  var found = false;
+  var traverseChildren = function (child) {
+    if (child.value === target) {
+      found = true;
+    } else {
+      for (var i = 0; i < child.children.length; i++) {
+        traverseChildren(child.children[i]);
+      }
+    }
+  }
+  traverseChildren(this);
+  return found;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+addChild = O(1)
+contains = O(n)
  */
